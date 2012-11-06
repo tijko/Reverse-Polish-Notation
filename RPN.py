e6fc4b8e5c88a8586c3e0e765f61ce5b94649b1e
tijko <oberon@oberon-Satellite-M645.(none)>
2012-11-06 16:45:01 -0500

    first

---
+++ RPN.py
@@ -0,0 +1,48 @@
+import sys
+
+def RPN(equation):
+    ops = {'(':1, '-':2, '+':2, '*':3, '/':3}
+    stack = []
+    out = []
+    for token in equation:
+        if token.isdigit():
+            out.append(token)
+        else:
+            if len(stack) == 1 and token in ops and ops[token] <= ops[stack[-1]] and token != '(':
+                out.append(stack[-1])
+                stack.pop(-1)
+                stack.append(token)
+            elif token == '(':
+                stack.append(token)
+            elif len(stack) == 1 and token in ops and ops[token] >= ops[stack[-1]]:
+                stack.append(token)
+            elif len(stack) > 1 and token in ops and ops[token] < ops[stack[-1]]:
+                out.append(stack[-1])
+                stack.pop(-1)
+                out.append(stack[-1])
+                stack.pop(-1)
+                stack.append(token)
+            elif token == ')':
+                while stack[-1] != '(':
+                    out.append(stack[-1])
+                    stack.pop(-1)
+                stack.pop(-1)
+            elif len(stack) > 1 and token in ops and ops[token] >= ops[stack[-1]] and stack[-1] != '(':
+                out.append(stack[-1])
+                stack.pop(-1)
+                stack.append(token)
+            elif len(stack) > 1 and stack[-1] == '(' and token in ops:
+                stack.append(token)
+            elif len(stack) < 1 and token in ops:
+                stack.append(token)
+    if len(stack) > 0:
+        sorted(stack, key=ops.get)
+        stack = stack[::-1]
+        for token in stack:
+            out.append(token)
+    fin = ''
+    for i in out:
+        fin += ' ' + i
+    print fin
+
+RPN(sys.argv[1])
